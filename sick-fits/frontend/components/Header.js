28053314a364cf77d5e52d0d58444c6ca785327e
OrkunSA <c.orkun.s@gmail.com>
2021-04-04 10:57:04 -0500

    add nprogress

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -21,22 +21,23 @@ const HeaderStyles = styled.header`
   .bar {
     border-bottom: 10px solid var(--black, black);
     display: grid;
-    border-bottom: 10px solid var(--black, black);
     grid-template-columns: auto 1fr;
     justify-content: space-between;
     align-items: center;
   }
-  sub-bar {
+  .sub-bar {
     display: grid;
     grid-template-columns: 1fr auto;
+    border-bottom: 1px solid var(--black, black);
   }
 `;
+
 export default function Header() {
   return (
     <HeaderStyles>
       <div className="bar">
         <Logo>
-          <Link href="/">Sick Fits</Link>
+          <Link href="/">Sick fits</Link>
         </Logo>
       </div>
       <div className="sub-bar">
