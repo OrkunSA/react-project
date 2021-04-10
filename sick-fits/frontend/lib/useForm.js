e9420e04e85ad78ff0a650376b2b31add70c6929
OrkunSA <c.orkun.s@gmail.com>
2021-04-09 20:11:24 -0500

    add form styles and hook up file input

--- sick-fits/frontend/lib/useForm.js
+++ sick-fits/frontend/lib/useForm.js
@@ -4,17 +4,22 @@ export default function useForm(initial = {}) {
   // create a state object for our inputs
   const [inputs, setInputs] = useState(initial);
 
+  // {
+  //   name: 'wes',
+  //   description: 'nice shoes',
+  //   price: 1000
+  // }
+
   function handleChange(e) {
     let { value, name, type } = e.target;
     if (type === 'number') {
       value = parseInt(value);
     }
-
     if (type === 'file') {
-      value[0] = e.target.files;
+      [value] = e.target.files;
     }
-
     setInputs({
+      // copy the existing state
       ...inputs,
       [name]: value,
     });
@@ -31,6 +36,7 @@ export default function useForm(initial = {}) {
     setInputs(blankState);
   }
 
+  // return the things we want to surface from this custom hook
   return {
     inputs,
     handleChange,
