d4f7bd2ee6c81b3da4f595664d1cbc022862937e
OrkunSA <c.orkun.s@gmail.com>
2021-05-16 17:56:18 +0300

    update keystone.ts

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -1,12 +1,12 @@
-import { config, createSchema } from '@keystone-next/keystone/schema';
 import { createAuth } from '@keystone-next/auth';
+import { config, createSchema } from '@keystone-next/keystone/schema';
 import {
   withItemData,
   statelessSessions,
 } from '@keystone-next/keystone/session';
+import { ProductImage } from './schemas/ProductImage';
 import { Product } from './schemas/Product';
 import { User } from './schemas/User';
-import { ProductImage } from './schemas/ProductImage';
 import 'dotenv/config';
 import { insertSeedData } from './seed-data';
 
@@ -14,7 +14,7 @@ const databaseURL =
   process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
 
 const sessionConfig = {
-  maxAge: 60 * 60 * 24 * 360,
+  maxAge: 60 * 60 * 24 * 360, // How long they stay signed in?
   secret: process.env.COOKIE_SECRET,
 };
 
@@ -24,7 +24,12 @@ const { withAuth } = createAuth({
   secretField: 'password',
   initFirstItem: {
     fields: ['name', 'email', 'password'],
-    // TODO: Add in initial roles here
+    // TODO: Add in inital roles here
+  },
+  passwordResetLink: {
+    async sendToken(args) {
+      console.log(args);
+    },
   },
 });
 
@@ -41,27 +46,27 @@ export default withAuth(
       adapter: 'mongoose',
       url: databaseURL,
       async onConnect(keystone) {
-        console.log('connected to db');
+        console.log('Connected to the database!');
         if (process.argv.includes('--seed-data')) {
           await insertSeedData(keystone);
         }
       },
     },
     lists: createSchema({
+      // Schema items go in here
       User,
       Product,
       ProductImage,
-
-      // Schema items go in here
     }),
     ui: {
-      // Show the UI only for people who pass this test
+      // Show the UI only for poeple who pass this test
       isAccessAllowed: ({ session }) =>
-        //   console.log(session)
-        session?.data,
+        // console.log(session);
+        !!session?.data,
     },
     session: withItemData(statelessSessions(sessionConfig), {
-      User: 'id',
+      // GraphQL Query
+      User: 'id name email',
     }),
   })
 );
